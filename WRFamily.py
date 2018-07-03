cf4598898aebc715e7e80f32b395fc3994f9c14e
WhiteRascals <40721340+WhiteRascals@users.noreply.github.com>
2018-07-03 12:48:09 +0700

    Update WRFamily.py

--- WRFamily.py
+++ WRFamily.py
@@ -1,14 +1,1 @@
-while True:
-    try:
-        ops = poll.singleTrace(count=50)
-        if ops is not None:
-            for op in ops:
-               # bot(op)
-                # Don't remove this line, if you wan't get error soon!
-                poll.setRevision(op.revision)
-                thread1 = threading.Thread(target=bot, args=(op,))#self.OpInterrupt[op.type], args=(op,)
-                #thread1.daemon = True
-                thread1.start()
-                thread1.join()
-    except Exception as e:
-        pass
+## NOTE FOUND 
